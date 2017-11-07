7e3f32f677995f2b47a4314c689146f0086133f1
MarionFoto <marionacp@gmail.com>
2017-11-07 19:52:11 -0300

    crear una pagina parecita a twitter

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+var buttonTweet = document.getElementById('btn');
+	buttonTweet.addEventListener('click', function() {
+	
+	//tenemos que tomar el texto ingresado en el textarea
+	var tweets = document.getElementById('tweets').value;
+	//limpiar el textarea
+	document.getElementById('tweets').value = '';
+
+	//contenedor que está en el html
+	var container = document.getElementById('container');
+
+	//creamos el div que contiene cada comentario
+	var newTweet = document.createElement('div');
+
+	//validar que textarea tenga un mensaje
+	if (tweets.length == 0 || tweets == null) {
+		return false;
+	} 
+	//nodo de texto del textarea
+	var textNewTweet = document.createTextNode(tweets);
+
+	//Pasamos elementos
+	var contenedorElemento = document.createElement('p');
+		contenedorElemento.appendChild(textNewTweet);
+		newTweet.appendChild(contenedorElemento);
+
+		container.appendChild(newTweet);	
+
+	//Llamando al textarea
+	var textArea = document.getElementsByTagName('textarea');

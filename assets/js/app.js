d992a59cd09bea78d5a5ae0978e40266e2254265
Carolina-Ruiz <cruizcornejo@gmail.com>
2017-12-31 18:14:27 -0300

    trate de dar funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,56 @@
-var buttonTweet = document.getElementById('btn');
-	buttonTweet.addEventListener('click', function() {
-	
-	//tenemos que tomar el texto ingresado en el textarea
-	var tweets = document.getElementById('tweets').value;
-	//limpiar el textarea
-	document.getElementById('tweets').value = '';
+//Declarar variables.
+var btn = document.getElementById('btn'); //boton
+var seccionTweets = document.getElementById("dos");
 
-	//contenedor que está en el html
-	var container = document.getElementById('container');
+//Cuando se haga click al botn hara la funcion:
+btn.addEventListener('click', function() {
+//1 tomar el texto ingresado en el textarea.
+var comentario = document.getElementById("comentario").value;
+//2 limpiar el textarea.
+document.getElementById("comentario").value = "";
 
-	//creamos el div que contiene cada comentario
-	var newTweet = document.createElement('div');
+/* 3 Si la variable comentario tiene NO es string vacio, 
+ * entonces aplica lo siguiente: creara un div, luego un parrafo, y comenatario*/
+  if (comentario !== "" && comentario !== " "){
+    var contParrafo = document.createElement("div");
+    var parrafo = document.createElement("p");
+    var textTweet = document.createTextNode(comentario);
+ //4 Se ordenaran de la siguiente manera
+    parrafo.appendChild(textTweet);
+    contenedorParrafo.appendChild(parrafo);
+    seccionTweets.appendChild(contParrafo);
+//5 Ingresa una clase para que luego se pueda editar el estilo.
+    contenedorParrafo.setAttribute("class", "posteo");
+//6 En caso contrario, pedir al usuario que ingrese algún texto */
+} else {
+    alert("Escriba su comentario");
+}
 
-	//validar que textarea tenga un mensaje
-	if (tweets.length == 0 || tweets == null) {
-		return false;
-	} 
-	//nodo de texto del textarea
-	var textNewTweet = document.createTextNode(tweets);
+});
 
-	//Pasamos elementos
-	var contenedorElemento = document.createElement('p');
-		contenedorElemento.appendChild(textNewTweet);
-		newTweet.appendChild(contenedorElemento);
+//contador
+var textArea = document.getElementById('comentario');
+//cuando el usuario presione teclas dentro de texArea hara la funcion:
+comenatrio.onkeydown = function(){
+	var comentario = document.getElementById('comentario');
+	var saveCommit = comentario.value.length;
+	var cont = document.getElementById('cont');
+	cont.innerHTML = 140 - saveCommit; 
+	//cambia el color los caracteres (blue, red)
+	if(saveCommit >= 120 && saveCommit <130){
+		return cont.setAttribute('class','blue');
+	}
+	if(saveCommit >= 130 && saveCommit < 140){
+		return cont.setAttribute('class','red');
+	} else{
+		
+	};
+	if(saveCommit > 140){
+		return document.getElementById('btn').disabled = true;
+	} else {
+		return document.getElementById('btn').enabled = true;
+	}
 
-		container.appendChild(newTweet);	
 
-	//Llamando al textarea
-	var textArea = document.getElementsByTagName('textarea');
+
+}
